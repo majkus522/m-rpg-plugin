9874f9acc231cb35fb948d981e8af5ff8ab152a0
majkus522 <mikohus522@gmail.com>
2023-07-27 11:04:07 +0200

    Implement status skills (missing file)

---
+++ src/main/java/pl/majkus522/mrpg/controllers/StatusController.java
@@ -0,0 +1,82 @@
+package pl.majkus522.mrpg.controllers;
+
+import com.google.gson.Gson;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import pl.majkus522.mrpg.Main;
+import pl.majkus522.mrpg.common.ExtensionMethods;
+import pl.majkus522.mrpg.common.classes.api.RequestErrorResult;
+import pl.majkus522.mrpg.common.classes.api.RequestPlayer;
+import pl.majkus522.mrpg.common.classes.api.RequestResult;
+
+public class StatusController
+{
+    public static void sendPlayerStatus(Player player)
+    {
+        if (!ExtensionMethods.isPlayerLogged(player))
+            return;
+        if(!(SkillsController.playerHasSkill(player, "statusPreview") || SkillsController.playerHasSkill(player, "statusVision")))
+            return;
+
+        RequestResult request = ExtensionMethods.httpRequest("GET", Main.mainUrl + "endpoints/players/" + player.getName(), player);
+        Gson gson = new Gson();
+        if(request.isOk())
+        {
+            RequestPlayer playerData = gson.fromJson(request.content, RequestPlayer.class);
+            player.sendMessage(ChatColor.BLUE + "=-=-=-=-= " + ChatColor.GREEN + "Status: " + player.getName() + ChatColor.BLUE + " =-=-=-=-=");
+            player.sendMessage("Strength: " + playerData.strength);
+            player.sendMessage("Agility: " + playerData.agility);
+            player.sendMessage("Charisma: " + playerData.charisma);
+            player.sendMessage("Intelligence: " + playerData.intelligence);
+            player.sendMessage(ChatColor.BLUE + "=-=-=-=-= " + ChatColor.GREEN + "Status: " + player.getName() + ChatColor.BLUE + " =-=-=-=-=");
+        }
+        else
+        {
+            player.sendMessage(gson.fromJson(request.content, RequestErrorResult.class).message);
+        }
+    }
+
+    public static void sendOtherPlayerStatus(Player sender, Player whose)
+    {
+        if (!(ExtensionMethods.isPlayerLogged(sender) || ExtensionMethods.isPlayerLogged(whose)))
+            return;
+        if(!(SkillsController.playerHasSkill(sender, "statusPreview") || SkillsController.playerHasSkill(sender, "statusVision")))
+            return;
+
+        RequestResult senderRequest = ExtensionMethods.httpRequest("GET", Main.mainUrl + "endpoints/players/" + sender.getName(), sender);
+        if(!senderRequest.isOk())
+        {
+            senderRequest.printError();
+            sender.sendMessage("Server error");
+            return;
+        }
+        RequestResult whoseRequest = ExtensionMethods.httpRequest("GET", Main.mainUrl + "endpoints/players/" + whose.getName(), whose);
+        if(!whoseRequest.isOk())
+        {
+            whoseRequest.printError();
+            sender.sendMessage("Server error");
+            return;
+        }
+        Gson gson = new Gson();
+        int level = gson.fromJson(senderRequest.content, RequestPlayer.class).level;
+        RequestPlayer playerData = gson.fromJson(whoseRequest.content, RequestPlayer.class);
+        int round = -1;
+        if(playerData.level - level > 5)
+            round = -2;
+        if (SkillsController.playerHasSkill(sender, "statusVision"))
+            round = 0;
+        sender.sendMessage(ChatColor.BLUE + "=-=-=-=-= " + ChatColor.GREEN + "Status: " + whose.getName() + ChatColor.BLUE + " =-=-=-=-=");
+        sender.sendMessage("Level: " + round(playerData.level, round));
+        sender.sendMessage("Money: " + round(playerData.money, round * 3));
+        sender.sendMessage("Strength: " + round(playerData.strength, round));
+        sender.sendMessage("Agility: " + round(playerData.agility, round));
+        sender.sendMessage("Charisma: " + round(playerData.charisma, round));
+        sender.sendMessage("Intelligence: " + round(playerData.intelligence, round));
+        sender.sendMessage(ChatColor.BLUE + "=-=-=-=-= " + ChatColor.GREEN + "Status: " + whose.getName() + ChatColor.BLUE + " =-=-=-=-=");
+    }
+
+    static String round(double value, int decimalPlace)
+    {
+        return (decimalPlace != 0 ? "~" : "") + ExtensionMethods.roundInt(value, decimalPlace);
+    }
+}
