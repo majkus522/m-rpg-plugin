7bdb8ae710fafeefadf1229312f19c75d4a57dd4
majkus522 <mikohus522@gmail.com>
2023-08-11 15:20:24 +0200

    NMS remaps install

--- src/main/java/pl/majkus522/mrpg/controllers/NBTController.java
+++ src/main/java/pl/majkus522/mrpg/controllers/NBTController.java
@@ -1,6 +1,6 @@
 package pl.majkus522.mrpg.controllers;
 
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.CompoundTag;
 import org.bukkit.craftbukkit.v1_20_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
@@ -9,19 +9,19 @@ public class NBTController
     public static ItemStack putNBTString(ItemStack item, String key, String value)
     {
         net.minecraft.world.item.ItemStack nmsCopy = CraftItemStack.asNMSCopy(item);
-        NBTTagCompound tag = nmsCopy.w();
-        tag.a(key, value);
-        nmsCopy.c(tag);
+        CompoundTag tag = nmsCopy.getOrCreateTag();
+        tag.putString(key, value);
+        nmsCopy.setTag(tag);
         return CraftItemStack.asBukkitCopy(nmsCopy);
     }
 
     public static String getNBTString(ItemStack item, String key)
     {
-        return CraftItemStack.asNMSCopy(item).w().l(key);
+        return CraftItemStack.asNMSCopy(item).getOrCreateTag().getString(key);
     }
 
     public static boolean hasNBTTag(ItemStack item, String key)
     {
-        return CraftItemStack.asNMSCopy(item).w().e(key);
+        return CraftItemStack.asNMSCopy(item).getOrCreateTag().contains(key);
     }
 }
