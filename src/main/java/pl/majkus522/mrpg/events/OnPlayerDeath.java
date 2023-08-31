1c16bb7cab7120519a893ae514e7c3f2709c83fe
majkus522 <mikohus522@gmail.com>
2023-08-31 15:47:04 +0200

    Fix #3

--- src/main/java/pl/majkus522/mrpg/events/OnPlayerDeath.java
+++ src/main/java/pl/majkus522/mrpg/events/OnPlayerDeath.java
@@ -1,9 +1,11 @@
 package pl.majkus522.mrpg.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import pl.majkus522.mrpg.Main;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -13,11 +15,19 @@ public class OnPlayerDeath implements Listener
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event)
     {
-        Player player = event.getEntity();
-        Date date = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.DATE, 1);
-        player.ban("You have died", calendar.getTime(), "death", true);
+        Bukkit.getScheduler().runTask(Main.plugin, new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Player player = event.getEntity();
+                player.spigot().respawn();
+                Date date = new Date();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                calendar.add(Calendar.DATE, 1);
+                player.ban("You have died", calendar.getTime(), "death", true);
+            }
+        });
     }
 }
